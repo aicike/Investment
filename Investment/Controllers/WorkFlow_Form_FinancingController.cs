d03fb4d12bd6592a0a11c276828b073ff48efe64
aicike <176534021@qq.com>
2015-01-09 09:46:02 +0800

    first commit

--- Investment/Controllers/WorkFlow_Form_FinancingController.cs
+++ Investment/Controllers/WorkFlow_Form_FinancingController.cs
@@ -20,16 +20,21 @@ namespace Investment.Controllers
             {
                 WorkFlowModel wfm = new WorkFlowModel();
                 var wf = wfm.Get(workflowID.Value);
-                //只有当未通过和已完成时，才从json(快照)中取数据
-                if (wf.State == 2 || wf.State == 3)
+                if (wf != null)
                 {
-                    financing = Newtonsoft.Json.JsonConvert.DeserializeObject<Financing>(wf.FormJson);
+                    //只有当未通过和已完成时，才从json(快照)中取数据
+                    if (wf.State == 2 || wf.State == 3)
+                    {
+                        financing = Newtonsoft.Json.JsonConvert.DeserializeObject<Financing>(wf.FormJson);
+                    }
                 }
             }
             if (financing == null)
             {
                 FinancingModel fm = new FinancingModel();
                 financing = fm.Get(financingID);
+
+
             }
             return PartialView(financing);
         }
