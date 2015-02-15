0c4d3831d609f26ac88d7cf448a553740e75c85f
gongmy <gmyao.china@gmail.com>
2015-02-15 10:25:33 +0800

    ads

--- Business/FinancingMatchingModel.cs
+++ Business/FinancingMatchingModel.cs
@@ -20,7 +20,7 @@ namespace Business
         {
             var query = from a in Context.Financings
                         from b in Context.MechanismProducts
-                        where a.WorkFlowManagerID != 4 && a.Amount <= b.MaxQuota && a.Owner_A_ID == AccountID && a.Status == 0 && (a.MinTimeLimit + a.MaxTimeLimit ?? 0) <= b.MaxMonth
+                        where a.WorkFlowManagerID != 4 && a.Amount <= b.MaxQuota && a.Owner_A_ID == AccountID && a.Status == 0 && (a.MinTimeLimit + a.MaxTimeLimit ?? 0) <= b.MaxMonth && a.AuditStatus==1
                         orderby a.ID
                         select new FinancingMatching { FID = a.ID, FName = a.Name, MID = b.ID, MName = b.Name };
             return query;
@@ -50,7 +50,7 @@ namespace Business
         {
             var query = from a in Context.Financings
                         from b in Context.MechanismProducts
-                        where a.WorkFlowManagerID != 4 && a.Amount <= b.MaxQuota && a.Status == 0 && (a.MinTimeLimit + a.MaxTimeLimit ?? 0) <= b.MaxMonth
+                        where a.WorkFlowManagerID != 4 && a.Amount <= b.MaxQuota && a.Status == 0 && (a.MinTimeLimit + a.MaxTimeLimit ?? 0) <= b.MaxMonth && a.AuditStatus == 1
                         orderby a.ID
                         select new FinancingMatching { FID = a.ID, FName = a.Name, MID = b.ID, MName = b.Name };
             return query;
